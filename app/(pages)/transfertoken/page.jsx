2fd4378562bc77b66266a71054850c64c322457a
codebyMoh <codebymohit@gmail.com>
2024-05-31 16:44:39 +0530

    :

--- app/(pages)/transfertoken/page.jsx
+++ app/(pages)/transfertoken/page.jsx
@@ -617,7 +617,7 @@ const TransferToken = () => {
                       key={index}
                       className={`flex gap-3 justify-start items-center mx-5 py-2 cursor-pointer ${
                         clickedTokens.includes(item.name)
-                       
+                   
                       }`}
                       onClick={() => {
                        {
