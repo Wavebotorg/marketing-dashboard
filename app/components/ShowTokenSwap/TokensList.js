2c94d3c7bd0208a88366f3fd4f6e49d7b26f08e5
codebyMoh <codebymohit@gmail.com>
2024-05-31 16:40:16 +0530

    transfer added

--- app/components/ShowTokenSwap/TokensList.js
+++ app/components/ShowTokenSwap/TokensList.js
@@ -12,12 +12,10 @@ const TokenList = ({ tokens = [], clickedTokens, selectToken, searchTerm }) => {
         <div
           key={index}
           className={`flex gap-3 justify-start items-center mx-5 py-2 cursor-pointer ${
-            clickedTokens.includes(token.name)
-              ? "opacity-50 cursor-not-allowed"
-              : ""
+            clickedTokens.includes(token.name) ? "opacity-50 " : ""
           }`}
           onClick={() => {
-            if (!clickedTokens.includes(token.name)) {
+            {
               selectToken(token);
             }
           }}
