82076a1aedd3922940bb52a3f06b8b934e77cabb
CodebyAnandi <anandi.tanthetaa@gmail.com>
2024-05-31 16:38:42 +0530

    buy sell done111

--- app/components/ShowTokenSwap/TokensList.js
+++ app/components/ShowTokenSwap/TokensList.js
@@ -13,11 +13,10 @@ const TokenList = ({ tokens = [], clickedTokens, selectToken, searchTerm }) => {
           key={index}
           className={`flex gap-3 justify-start items-center mx-5 py-2 cursor-pointer ${
             clickedTokens.includes(token.name)
-              ? "opacity-50 cursor-not-allowed"
-              : ""
+            
           }`}
           onClick={() => {
-            if (!clickedTokens.includes(token.name)) {
+          {
               selectToken(token);
             }
           }}
@@ -32,7 +31,7 @@ const TokenList = ({ tokens = [], clickedTokens, selectToken, searchTerm }) => {
           <div className="flex gap-2">
             <p
               className={`font-semibold ${
-                clickedTokens.includes(token.name) ? "text-gray-500" : ""
+                clickedTokens.includes(token.name) 
               }`}
             >
               {token.name}
