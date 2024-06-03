de67a1b70f413191c38fbcd9567e5ca5b60cadf3
codebyMoh <codebymohit@gmail.com>
2024-06-03 12:28:21 +0530

    commit code

--- app/(pages)/TransferToken/page.jsx
+++ app/(pages)/TransferToken/page.jsx
@@ -614,12 +614,11 @@ const TransferToken = () => {
                   showBalance?.map((item, index) => (
                     <div
                       key={index}
-                      className={`flex gap-3 justify-start items-center mx-5 py-2 cursor-pointer ${
-                        clickedTokens.includes(item.name)
-                   
-                      }`}
+                      className={`flex gap-3 justify-start items-center mx-5 py-2 cursor-pointer ${clickedTokens.includes(
+                        item.name
+                      )}`}
                       onClick={() => {
-                       {
+                        {
                           selectToken(item);
                         }
                       }}
