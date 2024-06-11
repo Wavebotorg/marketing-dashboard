03e83c42334ec7186b2620c760c75e5fa489986d
codebyMoh <codebymohit@gmail.com>
2024-06-11 09:48:16 +0530

    commit

--- app/(pages)/transfertoken/page.jsx
+++ app/(pages)/transfertoken/page.jsx
@@ -188,10 +188,10 @@ const TransferToken = () => {
             await getWalletBalance(selectedNetwork);
           }
         }, 3000);
+        resetFields(); // Reset all fields after successful transfer
       } else {
         toast.error(response?.data?.message);
       }
-      resetFields(); // Reset all fields after successful transfer
 
       // console.log(
       //   "------------------------------------------------------LOG",
@@ -236,10 +236,10 @@ const TransferToken = () => {
             await getWalletBalance(selectedNetwork);
           }
         }, 3000);
+        resetFields(); // Reset all fields after successful transfer
       } else {
         toast.error(response?.data?.message);
       }
-      resetFields(); // Reset all fields after successful transfer
     } catch (error) {
       console.error("There was a problem with the API call:", error);
       toast.error("An error occurred while processing your request");
