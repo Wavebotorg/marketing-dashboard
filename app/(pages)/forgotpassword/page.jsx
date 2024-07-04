cc6655e070a9fc01076f0d4b5a0c59f0ffe12dac
codebyMoh <codebymohit@gmail.com>
2024-07-04 18:15:18 +0530

    update in code

--- app/(pages)/forgotpassword/page.jsx
+++ app/(pages)/forgotpassword/page.jsx
@@ -126,7 +126,6 @@ const ForgotPassword = () => {
           </Link>
         </div>
       </div>
-      
     </div>
   );
 };
--- app/page.js
+++ app/page.js
@@ -21,15 +21,41 @@ const Page = () => {
   ];
 
   //for copy value
-  const [copied, setCopied] = useState(false);
-  const copyToClipboard = () => {
-    navigator.clipboard.writeText(userData?.userId);
-    setCopied(true);
-    setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
+  const [copied, setCopied] = useState(null);
+
+  const copyToClipboard = (ID, index) => {
+    navigator.clipboard.writeText(ID);
+    setCopied(index);
+    setTimeout(() => setCopied(null), 2000); // Reset copied state after 2 seconds
   };
 
   const { walletAddress, email, solanaAddress, isNavbar, setIsNavbar } =
     useWallet();
+
+  const formatTransactionID = (Id) => {
+    if (!Id || Id.length <= 10) return Id;
+    const firstSix = Id.slice(0, 5);
+    const lastFour = Id.slice(-5);
+    return `${firstSix}...${lastFour}`;
+  };
+
+  // let Email = localStorage.getItem("email");
+
+  // let ID = localStorage.getItem("userId");
+
+  const [isLargeScreen, setIsLargeScreen] = useState(false);
+
+  useEffect(() => {
+    const updateScreenSize = () =>
+      setIsLargeScreen(window.matchMedia("(min-width: 1024px)").matches);
+
+    updateScreenSize(); // Initial check
+
+    window.addEventListener("resize", updateScreenSize); // Listen to resize events
+
+    return () => window.removeEventListener("resize", updateScreenSize); // Clean up
+  }, []);
+
   return (
     <div
       style={{
@@ -40,71 +66,90 @@ const Page = () => {
       {/* 2xl:pl-64 xl:pl-64 */}
       <div className="text-white flex flex-col xl:justify-center      lg:ml-2 lg:mr-5 md:ml-0 xsm:ml-5 mr-5 mt-5">
         {/* xl:ml-32 xl:mr-[90px] */}
-        <div className="mx-3  md:flex gap-5 ">
+        <div className="mx-3  md:flex md:gap-20 lg:gap-0 ">
           <div className="flex items-center mb-5 md:mb-0">
             <Image src={HomeUser} width="20px" height="10px" alt="homeuser" />
             <span className="ml-5 mr-16 text-wrap md:text-nowrap">
+              {/* {Email} */}
               Anonymous-User-810b1
             </span>
           </div>
 
-          <div className="lg:container lg:mx- md:mx-6 mx-3  lg:border-l-2 lg:border-[#ffffff] lg:border-opacity-15 border-0 ">
-            <table className="responsive-table home-head-title ">
+          <div className="lg:container  lg:mx-10 md:mx-6 mx-3  lg:border-l-2 lg:border-[#ffffff] lg:border-opacity-15 border-0 ">
+            <table className="responsive-table home-head-title">
               <thead className="text-[#828282] md:text-[#828282]">
-                <tr className="   ">
+                <tr>
                   <th scope="col">User Id</th>
-                  <th scope="col ">VIP Level</th>
+                  <th scope="col">VIP Level</th>
                   <th scope="col">User Type</th>
                   <th scope="col">Following</th>
-                  <th scope="col ">Followers</th>
+                  <th scope="col">Followers</th>
                 </tr>
               </thead>
 
               <tbody>
-                {userData?.map((items) => (
-                  <>
-                    <tr className="md:pl-2 ">
-                      <td data-title="User Id" className="">
-                        {items?.userId}
-                        <button
-                          onClick={copyToClipboard}
-                          className="text-xl text-[#828282] align-middle pb-1.5 "
-                        >
-                          <MdOutlineContentCopy className="ml-1.5 items-center" />
-                        </button>
-                        {copied && (
-                          <span className="text-sm text-green-500 absolute top-full left-0">
-                            Copied!
-                          </span>
-                        )}
-                      </td>
-                      <td data-title="VIP Level" className="flex items-center ">
-                        <span className="ml-4">
-                          {items?.vipLevel}
-
-                          <button className=" text-xl  text-[#828282] align-middle  pb-0.6 ">
-                            <GrFormNext />
+                {userData?.map((items, index) => (
+                  <tr key={index} className="md:pl-2">
+                    <td data-title="User Id" className="">
+                      {isLargeScreen ? (
+                        <>
+                          {/* {ID} */}
+                          {items?.userId}
+                          <button
+                            onClick={() => copyToClipboard(ID)}
+                            className="text-xl text-[#828282] align-middle pb-1.5"
+                          >
+                            <MdOutlineContentCopy className="ml-1.5 items-center" />
                           </button>
-                        </span>
-                      </td>
+                          {copied === index && (
+                            <span className="text-sm bg-white text-green-500 absolute top-full left-0">
+                              Copied!
+                            </span>
+                          )}
+                        </>
+                      ) : (
+                        <>
+                          {/* {formatTransactionID(ID)} */}
+                          {items?.userId}
+                          <button
+                            onClick={() => copyToClipboard(ID)}
+                            className="text-xl text-[#828282] align-middle pb-1.5"
+                          >
+                            <MdOutlineContentCopy className="ml-1.5 items-center" />
+                          </button>
+                          {copied === index && (
+                            <span className="text-sm bg-white text-green-500 absolute top-full left-0">
+                              Copied!
+                            </span>
+                          )}
+                        </>
+                      )}
+                    </td>
 
-                      <td data-title="User Type" className=" ">
-                        {items?.userType}
-                      </td>
-                      <td data-title="Following" className=" ">
-                        {items?.following}
-                      </td>
-                      <td
-                        data-title="Followers"
-                        className=" flex items-center pt-5"
-                      >
-                        {items?.followers}
-                        <button className=" text-xl pt-[0.2rem] text-[#828282] align-middle pb-1.5 ">
+                    <td data-title="VIP Level" className="flex items-center">
+                      <span className="ml-4">
+                        {items.vipLevel}
+                        <button className="text-xl text-[#828282] align-middle pb-0.6">
                           <GrFormNext />
-                        </button>{" "}
-                      </td>
-                    </tr>
-                  </>
+                        </button>
+                      </span>
+                    </td>
+                    <td data-title="User Type" className="">
+                      {items.userType}
+                    </td>
+                    <td data-title="Following" className="">
+                      {items.following}
+                    </td>
+                    <td
+                      data-title="Followers"
+                      className="flex items-center pt-5"
+                    >
+                      {items.followers}
+                      <button className="text-xl pt-[0.2rem] text-[#828282] align-middle pb-1.5">
+                        <GrFormNext />
+                      </button>
+                    </td>
+                  </tr>
                 ))}
               </tbody>
             </table>
