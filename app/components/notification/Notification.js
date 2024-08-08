d1e767fee5b9dd68b11fb7d9b58ecf4a9134970a
CodebyMoh <codebymohit@gmail.com>
2024-08-08 17:17:46 +0530

    change in notification page

--- app/components/notification/Notification.js
+++ app/components/notification/Notification.js
@@ -69,7 +69,7 @@ const Notification = ({ isOpen, toggle, setIsOpen }) => {
   return (
     <div
       // ref={notificationRef}
-      className={`fixed inset-y-0 mt-[5.8rem] right-0 w-80 bg-gray-800 px-8 py-4 transition duration-300 ease-in-out transform ${
+      className={`fixed inset-y-0 mt-[5.8rem] right-0 w-80 bg-gray-800 px-8 py-4 transition duration-500 ease-in-out transform ${
         isOpen ? "translate-x-0" : "translate-x-full"
       }`}
     >
